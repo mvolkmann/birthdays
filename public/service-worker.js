4d7b8e22270f0f056d6a6f62f90af742d2ab9830
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2019-01-04 16:07:47 -0600

    moved pwa.md to pwa-demo project

--- public/service-worker.js
+++ public/service-worker.js
@@ -4,3 +4,19 @@ self.addEventListener('fetch', event => {
   const {url} = event.request;
   console.log('service-worker fetch: url =', url);
 });
+
+self.addEventListener('activate', event => {
+  console.log('service-worker activate entered');
+});
+
+self.addEventListener('install', event => {
+  console.log('service-worker install entered');
+});
+
+self.addEventListener('push', event => {
+  console.log('service-worker push entered');
+});
+
+self.addEventListener('sync', event => {
+  console.log('service-worker sync entered');
+});
